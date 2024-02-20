4db409f96f7860892092ff37f7a4cd846135b244
onlyshubham4u <onlyshubham4u@gmail.com>
2024-02-20 19:13:23 +0530

    My Portfolio
    Animated Head Portfolio

---
+++ js/color-mode.js
@@ -0,0 +1,36 @@
+// Define DOM elements
+
+
+// Load the user's preffered color mode from local storage.
+function loadColorMode() {
+  const colorMode = localStorage.getItem(storageKey);
+  root.classList.add(colorMode || defaultMode);
+  updateToggleButton();
+}
+
+loadColorMode();
+
+// Toggle the color mode
+toggleButton.addEventListener("click", () => {
+  saveColorMode();
+});
+
+// Save the users's preffered color mode to local storage
+function saveColorMode() {
+  // Check if the root element has a class of "dark-mode". If yes, then the current mode is switched to light, and vice versa.
+  const currentMode = root.classList.contains("dark-mode")
+    ? "light-mode"
+    : "dark-mode";
+  root.classList.remove("light-mode", "dark-mode");
+  root.classList.add(currentMode);
+  localStorage.setItem(storageKey, currentMode);
+  updateToggleButton();
+}
+
+function updateToggleButton() {
+  if (root.classList.contains("dark-mode")) {
+    toggleButton.style.backgroundImage = "var(--moon)";
+  } else {
+    toggleButton.style.backgroundImage = "var(--sun)";
+  }
+}
